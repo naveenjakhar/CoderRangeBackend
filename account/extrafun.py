d8705e950b03b7bf2bd32db902408b3f6d54a3a4
maheswarreddy peram <38140817+MaheshwarReddyPeram1@users.noreply.github.com>
2021-01-19 10:52:01 +0530

    Update extrafun.py

--- account/extrafun.py
+++ account/extrafun.py
@@ -1,12 +1,12 @@
 def send_sms(account_sid,auth_token,body,from_,to_):
     from twilio.rest import Client
     client=Client(account_sid ,auth_token)
-    message=client.messages.create(
+    message=client.messages \
+        .create(
             body=body,
             from_ =from_,
             to=to_
         )
-
 def ran_otp():
     import math,random
     digits='0123456789'
